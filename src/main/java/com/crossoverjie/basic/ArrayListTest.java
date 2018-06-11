f8e6bc859f4eab2e23eeda93d94a4157ac525cd1
zhaoyule <zhaoyule@wondersgroup.com>
2018-06-11 19:53:55 +0800

    ArrayList源码阅读

---
+++ src/main/java/com/crossoverjie/basic/ArrayListTest.java
@@ -0,0 +1,15 @@
+package com.crossoverjie.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List arrlist = new ArrayList();
+        //List list = new ArrayList(-1);
+        arrlist.add("name");
+        arrlist.add("zhaoyule");
+        System.out.println(arrlist.get(0));
+        System.out.println(arrlist.get(0));
+    }
+}
---
+++ src/main/java/com/crossoverjie/basic/SerialVersionUIDTest.java
@@ -0,0 +1,9 @@
+package com.crossoverjie.basic;
+
+import java.io.Serializable;
+
+public class SerialVersionUIDTest implements Serializable {
+
+    private static final long serialVersionUID = -4347819911630056053L;
+
+}
